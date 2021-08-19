a5c2afc990dec005b501444d89b6eb149c7531ed
FridmanS <86373775+FridmanS@users.noreply.github.com>
2021-08-19 20:56:27 +0300

    LaunchTest created

---
+++ src/test/java/testsScheduler/LaunchAppTest.java
@@ -0,0 +1,14 @@
+package testsScheduler;
+
+import config.ConfigScheduler;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pagesScheduler.SplashScreen;
+
+public class LaunchAppTest extends ConfigScheduler {
+    @Test
+    public void launchTest(){
+        String version = new SplashScreen(driver).getCurrentVersion();
+        Assert.assertEquals(version, "0.0.3");
+    }
+}
